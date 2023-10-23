6e6277dfb27bfa7c75fac0c22d8504a918a32816
milenapoteriko <106338663+milenapoteriko@users.noreply.github.com>
2023-10-23 17:31:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-function tocarTeclaGrilo(){
-    document.querySelector('#som_tecla_grilo').play();
+function tocarAplausos(){
+    document.querySelector('.som_tecla_aplausos').onclick = tocarAplausos;
 }
+function tocarVaia(){
+    document.querySelector('.som_tecla_vaia').onclick = tocarVaia;
+}
+function tocarRisada(){
+    document.querySelector('.som_tecla_risada').onclick = tocarRisada;
+}
+function tocarErrado(){
+    document.querySelector('.som_tecla_errado').onclick = tocarErrado;
+}
+function tocarGrilo(){
+    document.querySelector('.som_tecla_grilo').onclick = tocarGrilo;
+}
+function tocarTambores(){
+    document.querySelector('.som_tecla_tambores').onclick = tocarTambores
+}
+function tocarTrombeta(){
+    document.querySelector('.som_tecla_trombeta').onclick = tocarTrombeta
+}
+function tocarDinheiro(){
+    document.querySelector('.som_tecla_dinheiro').onclick = tocarDinheiro
+}
+function tocarVitoria(){
+    document.querySelector('.som_tecla_vitoria').onclick = tocarVitoria
+}
+
